a41898f973a44fe18294869f3b251ca52fa4f2cc
Viee.codes <161074194+Vipulsnips@users.noreply.github.com>
2025-12-17 18:28:22 +0530

    Time: 0 ms (100%), Space: 17.2 MB (62.23%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& a) {
+        int profit=0;
+        for(int i=0;i<a.size()-1;i++){
+            if(a[i] < a[i+1] ) profit += a[i+1] -a[i];
+        }
+        return profit;
+    }
+};
