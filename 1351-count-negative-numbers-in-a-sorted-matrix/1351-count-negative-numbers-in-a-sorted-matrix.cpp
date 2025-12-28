6ce3e719704e5be73bf0996336fdd0b42599a33e
Viee.codes <161074194+Vipulsnips@users.noreply.github.com>
2025-12-28 22:35:20 +0530

    Time: 1 ms (5.46%), Space: 14.8 MB (7.93%) - LeetHub

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        long long ans=0;
+        for(auto i:grid){
+            for(auto j:i) if(j<0) ans++;
+        }
+        return ans;
+    }
+};
