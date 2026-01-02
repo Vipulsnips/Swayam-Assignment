03f5e71078f80c3a7a8cf4d8ef7d45645be82d1c
Viee.codes <161074194+Vipulsnips@users.noreply.github.com>
2026-01-02 22:35:36 +0530

    Time: 27 ms (8.36%), Space: 35.1 MB (6.44%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        map <int,int > mp;
+        for(auto i:nums) mp[i]++;
+        for(auto i:mp){
+            if(i.second== (nums.size())/2) return i.first;
+        }
+        return 0;
+    }
+};
